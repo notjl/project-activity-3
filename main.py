e5d5f141978dc35755eddc2a661fa9d68292c05a
Jan Angelo Paras <95292685+qjabparas@users.noreply.github.com>
2022-10-26 08:47:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import requests
 EXTERNAL_V4_API = "https://ident.me/"
 
 resp = requests.get(EXTERNAL_V4_API)
-external_v4 = resp.content.decode("utf8")
+external_v4 = resp.content.decode("utf8") // To remove binary string
 
-GEOLOCATION_API = f"https://ipapi.co/{external_v4}/json"
+GEOLOCATION_API = f"https://ipapi.co/{external_v4}/json" //Getting the API Response
 
 resp = requests.get(GEOLOCATION_API)
-geolocation_deets = resp.json()
+geolocation_deets = resp.json() //Converting into .json file 
+
+//Printing API Response
 print("=" * 50)
 print(f"External IPv4 Address:\t\t{geolocation_deets['ip']}")
 print(f"Internet Service Provide:\t{geolocation_deets['org']}")
