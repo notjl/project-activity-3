b0a79a115f2bd0f944e372e8a076d62ed56ca24c
notjl <qagpcabezas@tip.edu.ph>
2022-10-26 00:29:00 +0000

    Project Activity main code

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+EXTERNAL_V4_API = "https://ident.me/"
+
+resp = requests.get(EXTERNAL_V4_API)
+external_v4 = resp.content.decode("utf8")
+
+GEOLOCATION_API = f"https://ipapi.co/{external_v4}/json"
+
+resp = requests.get(GEOLOCATION_API)
+geolocation_deets = resp.json()
+print("=" * 50)
+print(f"External IPv4 Address:\t\t{geolocation_deets['ip']}")
+print(f"Internet Service Provide:\t{geolocation_deets['org']}")
+print(f"Autonomous System Number:\t{geolocation_deets['asn']}")
+print(f"Country Code:\t\t\t{geolocation_deets['country_code']}")
+print(f"Latitude / Longitude:\t\t{geolocation_deets['latitude']}, {geolocation_deets['longitude']}")
+print("=" * 50)
