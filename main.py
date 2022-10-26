43cd0c67d9f19ef283e1ddd784a4ec315d97a97f
notjl <qagpcabezas@tip.edu.ph>
2022-10-26 01:02:42 +0000

    IPv6 Functionality Added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import requests
+from contextlib import suppress
 
 EXTERNAL_V4_API = "https://ident.me/"
+EXTERnAL_V6_API = "https://v6.ident.me"
 
 resp = requests.get(EXTERNAL_V4_API)
 external_v4 = resp.content.decode("utf8") #To remove binary string
+
+# Try the v6 API endpoint
+with suppress(Exception):
+    resp_v6 = requests.get(EXTERNAL_V6_API)
+    external_v6 = resp.content.decode("utf8")
 
 GEOLOCATION_API = f"https://ipapi.co/{external_v4}/json" #Getting the API Response
 
@@ -13,6 +20,11 @@ geolocation_deets = resp.json() #Converting into .json file
 #Printing API Response
 print("=" * 50)
 print(f"External IPv4 Address:\t\t{geolocation_deets['ip']}")
+
+# Print the IPv6 Address if it is available
+with suppress(Exception):
+    print(f"External IPv6 Address:\t\t{external_v6}")
+
 print(f"Internet Service Provide:\t{geolocation_deets['org']}")
 print(f"Autonomous System Number:\t{geolocation_deets['asn']}")
 print(f"Country Code:\t\t\t{geolocation_deets['country_code']}")
